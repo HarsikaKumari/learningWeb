931946912d42e135f8f4a7a335929e4f62c3bb97
Harsika Kumari <kumariharsika8@gmail.com>
2024-06-23 23:29:07 +0530

    starting with basics of usestate

---
+++ hooks/src/StateHook.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function StateHook() {
+  return (
+    <div>
+        <button> - </button>
+        <span> 0 </span>
+        <button> + </button>
+    </div>
+  )
+}
+
+export default StateHook;
