7d208fec969e6480bd05398506b27a8c6c53ef19
Harsika Kumari <kumariharsika8@gmail.com>
2024-06-24 18:41:47 +0530

    basics of usestate

--- hooks/src/StateHook.js
+++ hooks/src/StateHook.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react';
 
 function StateHook() {
+  const [count, setCount] = useState(2)
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
     <div>
-        <button> - </button>
-        <span> 0 </span>
-        <button> + </button>
+        <button onClick = {decrementCount}> - </button>
+        <span> {count} </span>
+        <button onClick = {incrementCount}> + </button>
     </div>
   )
 }
