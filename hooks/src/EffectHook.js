cb4979e5c5212c311b02766fe93f8a513b5cf229
Harsika Kumari <kumariharsika8@gmail.com>
2024-06-25 15:49:02 +0530

    using event listener 'onMOunt' in useEffect

--- hooks/src/EffectHook.js
+++ hooks/src/EffectHook.js
@@ -1,33 +1,51 @@
 import React, { useEffect, useState } from 'react'
 
 function EffectHook() {
-    const [resourceType, setResourceType] = useState('posts');
-    const [items, setItems] = useState([]);
 
-    console.log("render");
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    }
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-            .then(response => response.json())
-            .then(json => setItems(json))
-            .catch(() => setItems([]));
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+    // const [resourceType, setResourceType] = useState('posts');
+    // const [items, setItems] = useState([]);
+
+    // console.log("render");
+
+    // useEffect(() => {
+    //     fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    //         .then(response => response.json())
+    //         .then(json => setItems(json))
+    //         .catch(() => setItems([]));
             
-    }, [resourceType]) //the second param ensure the code is executed when this is changed.
+    // }, [resourceType]) //the second param ensure the code is executed when this is changed.
 
     return (
         <div>
-            <div>
-                <button onClick={() => { setResourceType('posts') }}>Posts</button>
-                <button onClick={() => { setResourceType('users') }}>Users</button>
-                <button onClick={() => { setResourceType('comments') }}>Comments</button>
-            </div>
-            <h1> {resourceType} </h1>
-            {
-                items.map(item => {
-                    return <pre>{JSON.stringify(item)}</pre>
-                })
-            }
+            {windowWidth}
         </div>
+        // <div>
+        //     <div>
+        //         <button onClick={() => { setResourceType('posts') }}>Posts</button>
+        //         <button onClick={() => { setResourceType('users') }}>Users</button>
+        //         <button onClick={() => { setResourceType('comments') }}>Comments</button>
+        //     </div>
+        //     <h1> {resourceType} </h1>
+        //     {
+        //         items.map(item => {
+        //             return <pre>{JSON.stringify(item)}</pre>
+        //         })
+        //     }
+        // </div>
     )
 }
 
