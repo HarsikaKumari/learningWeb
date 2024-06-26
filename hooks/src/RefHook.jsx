482a9d1f1221d6f8da45a5ec8a2e84e14efb2167
Harsika Kumari <kumariharsika8@gmail.com>
2024-06-26 16:43:51 +0530

    learn useRef

--- hooks/src/RefHook.jsx
+++ hooks/src/RefHook.jsx
@@ -1,12 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 function RefHook() {
     const [name, setName] = useState("")
+    // const renderCount = useRef(0)
+    //{current : 0}
+    const inputRef = useRef()
+    
+    // useEffect(() => {
+        // renderCount.current = renderCount.current + 1
+    // })
+
+    function focus() {
+        inputRef.current.focus()
+    }
 
     return (
         <div>
-            <input value={name} onChange={(e) => setName(e.target.value)} />
+            <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} />
             <div> My name is {name}</div>
+            {/* <div> I render it {inputRef.current} times.</div> */}
+            <button onClick={focus}>Focus</button>
         </div>
     )
 }
