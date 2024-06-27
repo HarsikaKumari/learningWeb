9ca54683a25b4b3b2332cf960de2587c48c53a52
Harsika Kumari <kumariharsika8@gmail.com>
2024-06-27 23:49:04 +0530

    learn useRef

--- hooks/src/RefHook.jsx
+++ hooks/src/RefHook.jsx
@@ -1,25 +1,31 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 
 function RefHook() {
     const [name, setName] = useState("")
     // const renderCount = useRef(0)
     //{current : 0}
-    const inputRef = useRef()
+    // const inputRef = useRef()
+    const prevName = useRef('')
     
     // useEffect(() => {
         // renderCount.current = renderCount.current + 1
     // })
 
-    function focus() {
-        inputRef.current.focus()
-    }
+    useEffect(() => {
+        prevName.current = name
+    }, [name])
+
+    // function focus() {
+    //     inputRef.current.focus()
+    // }
 
     return (
         <div>
-            <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} />
-            <div> My name is {name}</div>
+            {/* ref={inputRef} */}
+            <input value={name} onChange={(e) => setName(e.target.value)} />  
+            <div> My name is {name} and previously it used to be {prevName.current}</div>
             {/* <div> I render it {inputRef.current} times.</div> */}
-            <button onClick={focus}>Focus</button>
+            {/* <button onClick={focus}>Focus</button> */}
         </div>
     )
 }
