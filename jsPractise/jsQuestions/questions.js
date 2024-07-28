337ec8e2bf1efe0730e58ad8354c9a2280c6f9b4
Harsika Kumari <kumariharsika8@gmail.com>
2024-07-28 13:00:12 +0530

    practising question(1) on js map filter and reduce

---
+++ jsPractise/jsQuestions/questions.js
@@ -0,0 +1,14 @@
+// map filter reduce output based questions 
+// return the name of the students in upper case
+
+const students = [
+    {name: "Piyush", rollNo: 2, marks: 45},
+    {name: "Shreya", rollNo: 3, marks: 79},
+    {name: "Sarah", rollNo: 4, marks: 72},
+    {name: "Priya", rollNo: 5, marks: 82},
+    {name: "Sheetal", rollNo: 6, marks: 99},
+];
+
+const names = students.map((stu) => stu.name.toUpperCase());
+
+console.log(names);
