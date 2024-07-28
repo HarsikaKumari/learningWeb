5577ea3806b6280c55abbeaadb8d8fd82ec62352
Harsika Kumari <kumariharsika8@gmail.com>
2024-07-28 13:49:32 +0530

    practising question(4) on js map filter and reduce

--- jsPractise/jsQuestions/questions.js
+++ jsPractise/jsQuestions/questions.js
@@ -2,11 +2,11 @@
 // return the name of the students in upper case
 
 const students = [
-    {name: "Piyush", rollNo: 2, marks: 45},
-    {name: "Shreya", rollNo: 3, marks: 79},
-    {name: "Sarah", rollNo: 4, marks: 72},
-    {name: "Priya", rollNo: 5, marks: 82},
-    {name: "Sheetal", rollNo: 6, marks: 99},
+    { name: "Piyush", rollNo: 2, marks: 45 },
+    { name: "Shreya", rollNo: 3, marks: 79 },
+    { name: "Sarah", rollNo: 4, marks: 72 },
+    { name: "Priya", rollNo: 5, marks: 82 },
+    { name: "Sheetal", rollNo: 6, marks: 99 },
 ];
 
 // const names = students.map((stu) => stu.name.toUpperCase());
@@ -23,6 +23,23 @@ const students = [
 
 //sum of marks of all of the students
 
-const result = students.reduce((acc, current) => acc + current.marks, 0);
+// const result = students.reduce((acc, current) => acc + current.marks, 0);
 
-console.log(result);
+// console.log(result);
+
+//return only names of students who scored more then 80
+
+// const names = students.filter((stu) => stu.marks > 80).map(student => student.name);
+
+// console.log(names);
+
+//return total marks for the students with marks greater then 80 after adding 20 marks to the students with scored less then 80
+
+const totalMarks = students.map((stu) => {
+    if (stu.marks < 80) {
+        stu.marks += 20;
+    }
+    return stu;
+}).filter((stu) => stu.marks > 80).reduce((acc, curr) => acc + curr.marks, 0);
+
+console.log(totalMarks);
