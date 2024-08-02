79e6c0add61cd1c3d669cec70d90ac9dd23b9575
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-02 23:07:25 +0530

    learning closure scoop chain in js

--- jsPractise/jsQuestions/closure.js
+++ jsPractise/jsQuestions/closure.js
@@ -10,15 +10,33 @@
 // }
 // init();
 
-function makeFunction() {
-    var name = "harsika";
-    function displayFun(num) {
-        console.log(name, num);
-    }
-    return displayFun;
-}
+// function makeFunction() {
+//     var name = "harsika";
+//     function displayFun(num) {
+//         console.log(name, num);
+//     }
+//     return displayFun;
+// }
 
 // var myFun = makeFunction();
 // myFun();
 
-makeFunction()(17);
+// makeFunction()(17);
+
+// closure scope chain:-
+
+// global scope
+const e = 10;
+function sum(a) {
+    return function (b) {
+        return function (c) {
+            // outer functions scope
+            return function (d) {
+                // local scope
+                return a + b + c + d + e;
+            };
+        };
+    };
+}
+
+console.log(sum(1)(2)(3)(4)); // 20
