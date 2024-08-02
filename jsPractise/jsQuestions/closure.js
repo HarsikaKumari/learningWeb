0f9c00872cabbf34318798da171a544129df7139
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-02 22:58:20 +0530

    learning about lexical scoop and closures in js

--- jsPractise/jsQuestions/closure.js
+++ jsPractise/jsQuestions/closure.js
@@ -1,12 +1,22 @@
 // closure in js
 
-function init() {
-    var name = "Mozilla"; // name is a local variable created by init
-    function displayName() {
-        // displayName() is the inner function, that forms the closure
-        console.log(name); // use variable declared in the parent function
-    }
-    displayName();
-}
-init();
+// function init() {
+//     var name = "Mozilla"; // name is a local variable created by init
+//     function displayName() {
+// displayName() is the inner function, that forms the closure
+//         console.log(name); // use variable declared in the parent function
+//     }
+//     displayName();
+// }
+// init();
 
+function makeFunction() {
+    var name = "harsika";
+    function displayFun() {
+        console.log(name);
+    }
+    return displayFun;
+}
+
+var myFun = makeFunction();
+myFun();
