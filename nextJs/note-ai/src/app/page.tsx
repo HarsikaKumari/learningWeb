982aac656d70ec242d7cbed0e90305ff482c2697
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-07 22:27:31 +0530

    learning next basics

---
+++ nextJs/note-ai/src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Header } from "./Header";
+// import { Tweet } from 'react-tweet';
+
+export default function page() {
+  return (
+    <section>
+      <Header>
+        <h2>Helloo!</h2>
+      </Header>
+      <p>Hello!! This is my web page using Next</p>
+      {/* <Tweet id="1629307668568633344" /> */}
+    </section>
+  );
+}
