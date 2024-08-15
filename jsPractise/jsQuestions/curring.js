331ef35b681220bf64ebdb07878e33f5b8e1d0de
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-15 20:20:38 +0530

    practising questions(1) on currying in js

--- jsPractise/jsQuestions/curring.js
+++ jsPractise/jsQuestions/curring.js
@@ -1,6 +1,7 @@
 // curring in js
 // from fn(a, b) into fn(a)(b)
 
+/*
 function f(a) {
     return function(b) {
         return `${a} ${b}`;
@@ -8,4 +9,15 @@ function f(a) {
 }
 
 console.log(f(5)(4));
+*/
+
+function sum(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c;
+        };
+    };
+}
+
+console.log(sum(3)(5)(6));
 
