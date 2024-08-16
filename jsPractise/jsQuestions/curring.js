4a4afc10b5608876afd8069b992e17ae180c25e5
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-16 19:07:11 +0530

    practising questions(2) on currying in js

--- jsPractise/jsQuestions/curring.js
+++ jsPractise/jsQuestions/curring.js
@@ -11,6 +11,7 @@ function f(a) {
 console.log(f(5)(4));
 */
 
+/*
 function sum(a) {
     return function(b) {
         return function(c) {
@@ -20,4 +21,29 @@ function sum(a) {
 }
 
 console.log(sum(3)(5)(6));
+*/
 
+/* Question 2:-
+ evaluate("sum")(2)(3) => 5
+ evaluate("multiply")(2)(3) => 5
+ evaluate("divide")(2)(3) => 5
+ evaluate("subtract")(2)(3) => 5
+*/
+
+function evaluate(operation) {
+    return function(a) {
+        return function(b) {
+            if (operation === "sum") return a + b;
+            else if (operation === "multiply") return a * b;
+            else if (operation === "divide") return a / b;
+            else if (operation === "subtract") return a - b;
+            else return "invalid operation";
+        };
+    };
+}
+
+console.log(evaluate("sum")(2)(3));
+console.log(evaluate("multiply")(2)(3));
+console.log(evaluate("divide")(2)(3));
+console.log(evaluate("subtract")(2)(3));
+console.log(evaluate(" ")(2)(3));
