74c979807d310b4d60f9d5271325399a50683b67
Harsika Kumari <kumariharsika8@gmail.com>
2024-08-20 22:29:26 +0530

    pracise object questions(8) in js

--- jsPractise/jsQuestions/objectNew.js
+++ jsPractise/jsQuestions/objectNew.js
@@ -14,9 +14,17 @@ const { fullName: {firstName} } = user;
 console.log(firstName);
 */
 
+/*
 function getItem(fruitName, favFruit, ...args) {
     return [...fruitName, ...args, favFruit];
 }
 
 console.log(getItem(["banana", "orange"], "mango", "pear"));
+*/
 
+let c = {greeting: "hey"};
+let d;
+
+d = c; // we does not copy all the property in c, we just provide reference to c, so any change in c or d would affect both of the object
+c.greeting = "Hello";
+console.log(d.greeting); // hello
