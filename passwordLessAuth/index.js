21287e4bf960411ce8a94b0219f304cefb378d15
Harsika Kumari <kumariharsika8@gmail.com>
2024-10-16 23:20:18 +0530

    create server side and client side challenge

--- passwordLessAuth/index.js
+++ passwordLessAuth/index.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import crypto from "node:crypto";
+import {
+    generateRegistrationOptions,
+    // verifyRegistrationResponse
+} from '@simplewebauthn/server';
+
+if (!globalThis.crypto) {
+    globalThis.crypto = crypto;
+}
 
 const PORT = 3000 || 0;
 const app = express();
@@ -7,21 +16,47 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 const userStore = {}
+const challengeStore = {}
 
-app.post("./register", (req, res) => {
-    const{username, password} = req.body;
-    const id = `user_${Date.now()}`
+app.post("/register", (req, res) => {
+    const { username, password } = req.body;
+    const Id = `user_${Date.now()}`
 
-const user = {
-    id,
-    username,
-    password
-}
+    const user = {
+        Id,
+        username,
+        password
+    }
 
-userStore[id] = user;
-console.log("user registered successfully");
-return res.json({ id });
+    userStore[Id] = user;
+    console.log("user registered successfully", userStore[Id]);
+    return res.json({ Id });
 
+})
+
+app.post("/register-challenge", async (req, res) => {
+    const { userId } = req.body;
+
+    // console.log(req.body);
+// })
+// app.post("/register-challenge", async (error, req, res, next) => {
+//     const { userId } = req.body;
+//     console.log(userId);
+    
+
+    if (!userStore[userId]) return res.status(404).json({ error: "user not found!" })
+
+    const user = userStore[userId];
+    const challengePayload = await generateRegistrationOptions({
+        rpId: 'localhost',
+        rpName: 'My localhost',
+        username: user.username,
+    })
+    
+    console.log(challengePayload);
+    
+    challengeStore[userId] = challengePayload.challenge
+    return res.json({ options: challengePayload })
 })
 
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
